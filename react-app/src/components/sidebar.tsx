959d68ed19c8af671a109c76176054c4fe4a93f1
Mayadah <97852885+MayadahAA@users.noreply.github.com>
2023-08-31 11:27:56 +0300

    commit

---
+++ react-app/src/components/sidebar.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function sidebar() {
+  return (
+    <>
+    </>
+  )
+}
+
+export default sidebar
--- react-app/src/pages/Home.tsx
+++ react-app/src/pages/Home.tsx
@@ -1,10 +1,10 @@
-
+import Sidebar from "../components/sidebar";
 function Home() {
   return (
-    <h1 className="text-white text-lg">
-        lk,l,lk,k
-     
-    </h1>
+    < >
+         
+        <Sidebar/>
+    </>
   );
 }
 
