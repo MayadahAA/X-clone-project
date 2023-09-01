8d50f9442b0148b62e9aa9ea72ff54295cf05d11
Mohammed <dev.mjhh@gmail.com>
2023-09-01 12:27:15 +0300

    Edit filse of pages & add Func login&signUp

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,6 +1,9 @@
 import './001-Css/App.css'
 import { Routes, Route } from 'react-router-dom'
 import Home from './Pages/Home'
+import Profile from './Pages/Profile'
+import Signup from './Pages/Users/Signup'
+import Login from './Pages/Users/login'
 
 
 function App() {
@@ -9,6 +12,10 @@ function App() {
     <>
     <Routes>
       <Route path='/' element={<Home/>} />
+      <Route path='/profile' element={<Profile/>} />
+      <Route path='/profile/:id' element={<Profile/>} />
+      <Route path='/login' element={<Login/>} />
+      <Route path='/login' element={<Signup/>} />
     </Routes>
    
     </>
--- react-app/src/pages/Home.tsx
+++ react-app/src/pages/Home.tsx
@@ -6,7 +6,7 @@ import Sidebar from "../Components/sidebar";
 
 function Home() {
   return (
-    < >
+    <>
       <div className="flex w-full m-1">
         <div className="w-1/4 h-screen bg-pink-600"> <Navbar /></div>
 
@@ -14,7 +14,6 @@ function Home() {
           <div className=" h-32  mb-2 "><HeaderCard /></div>
           <div className="m-3"></div>
           <div className=" h-32  mb-2 mt-1"><TweetCard /></div>
-
         </div>
 
         <div className="w-1/3 bg-blue-700"> <Sidebar /></div>
