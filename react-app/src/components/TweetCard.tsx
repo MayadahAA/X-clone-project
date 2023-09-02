51d4e4ccfb1f27501ff7f65646bee73518507623
Mayadah <97852885+MayadahAA@users.noreply.github.com>
2023-09-02 14:33:37 +0300

    add icons

--- react-app/src/components/TweetCard.tsx
+++ react-app/src/components/TweetCard.tsx
@@ -36,7 +36,12 @@ export default function TweetCard() {
 
               {/* Button Action => "Delete" */}
               <div className="">
-                <p>icon</p>
+                <div>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+</svg>
+
+                </div>
               </div>
             </div>
 
@@ -49,8 +54,17 @@ export default function TweetCard() {
 
             {/* Action icons => "comment ,Retweet  , like , share" */}
             <div className="flex justify-around w-2/3 ml-4 pt-4">
-              <p>icon</p>
-              <p>icon</p>
+              <p>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 01-.923 1.785A5.969 5.969 0 006 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337z" />
+</svg>
+
+              </p>
+              <p>
+              <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 18">
+    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 14 3-3m-3 3 3 3m-3-3h16v-3m2-7-3 3m3-3-3-3m3 3H3v3"/>
+  </svg>
+              </p>
               <button>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
